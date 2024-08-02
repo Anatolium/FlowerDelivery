95e91bc23a969af985aff8f14e5d5fa53495f0f9
Анатолий <lan2002@yandex.ru>
2024-08-02 07:24:47 +0300

    (update) Add new pages

--- FlowerDelivery/FlowerDeliveryApp/views.py
+++ FlowerDelivery/FlowerDeliveryApp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("<h2>Это мой итоговый проект на Django</h2>")
+    # return HttpResponse("<h1>Это мой итоговый проект на Django</h1>")
+    return render(request, 'FlowerDeliveryApp/index.html')
 
 
 def new(request):
-    return HttpResponse("<h2>Это вторая страница моего итогового проекта на Django</h2>")
-
+    # return HttpResponse("<h2>Это вторая страница моего итогового проекта на Django</h2>")
+    return render(request, 'FlowerDeliveryApp/new.html')
